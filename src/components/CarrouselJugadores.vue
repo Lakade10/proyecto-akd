c1229611dac15cf988a1dce52a2dd9acc6e99512
Thiago Santero <santerothiago@gmail.com>
2024-05-27 17:50:53 -0300

    Añadidos carrouseles de jugadores y productos en el HomeView

--- src/components/CarrouselJugadores.vue
+++ src/components/CarrouselJugadores.vue
@@ -1,13 +1,117 @@
 <template>
-    <div class="carrousel-container">
-        
+  <div class="carousel-container">
+    <button @click="decrementarJugador" class="btn-arrow left">&#8249;</button>
+    <div class="carousel-track-container">
+      <h2 class="carousel-track-title">{{ titulo }}</h2>
+      <div class="carousel-track" :style="trackStyle">
+        <slot name="contenido" :items="jugadoresVisibles"></slot>
+      </div>
     </div>
+    <button @click="incrementarJugador" class="btn-arrow right">&#8250;</button>
+  </div>
 </template>
 
 <script setup>
+import { ref, defineProps, computed, onBeforeMount, onUnmounted } from 'vue';
+
+const props = defineProps(['jugadores', 'titulo'])
+
+const index = ref(0);
+const jugadoresPorPagina = ref(getJugadoresPorPagina());
+
+const jugadoresVisibles = computed(() => {
+  return props.jugadores//.slice(index.value, index.value + jugadoresPorPagina);
+});
+
+const incrementarJugador = () => {
+  if (index.value + jugadoresPorPagina.value < props.jugadores.length) {
+    index.value++;
+  }
+};
+
+const decrementarJugador = () => {
+  if (index.value > 0) {
+    index.value--;
+  }
+};
+
+const trackStyle = computed(() => {
+  return {
+    transform: `translateX(-${index.value * (100 / jugadoresPorPagina.value)}%)`,
+  };
+});
+
+function getJugadoresPorPagina() {
+  const width = window.innerWidth;
+  if (width > 1120) return 5;
+  if (width > 700) return 4;
+  if (width > 520) return 3;
+  return 2;
+}
+
+function handleResize() {
+  jugadoresPorPagina.value = getJugadoresPorPagina();
+}
+
+onBeforeMount(() => {
+  window.addEventListener('resize', handleResize);
+  handleResize(); // Call initially to set the correct value
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
 
 </script>
 
 <style scoped>
+.carousel-container {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+
+.carousel-track-container {
+  overflow: hidden;
+  width: 100%;
+}
+
+.carousel-track-title {
+  border-bottom: 5px groove rgb(80, 194, 236);
+  text-align: left;
+  padding-left: 20px;
+}
+
+.carousel-track {
+  display: flex;
+  transition: transform 0.5s ease;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+}
+
+.btn-arrow {
+  font-size: 2rem;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  z-index: 1;
+}
+
+.btn-arrow:hover {
+  background: rgba(0, 0, 0, 0.8);
+}
+
+.btn-arrow.left {
+  left: 0;
+}
+
+.btn-arrow.right {
+  right: 0;
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,41 @@
       <FixtureResumenComponent :partidos="partidos" :partidosPorPagina="2"/>
     </div>
   </div>
+  <div class="carousel-jugadores">
+    <CarrouselJugadoresComponent :jugadores="jugadores" :titulo="'PLANTEL PROFESIONAL'">
+      <template #contenido="slotProps">
+        <div v-for="jugador in slotProps.items" :key="jugador.id" class="jugador-card">
+          <a class="jugador-image-container" :href="`#/estadisticas/${jugador.id}`">
+            <img :src="jugador.imagen" :alt="jugador.nombre" class="jugador-image">
+            <div class="hover-overlay"><b>Ver Datos</b></div>
+          </a>
+          <h3 style="text-wrap: nowrap; margin-top: 10px;"><span style="color: gray">{{ '#' + jugador.dorsal + ' - '}}</span>{{ jugador.nombre }}</h3>
+        </div>
+      </template>
+    </CarrouselJugadoresComponent>
+  </div>
+  <div class="carousel-productos">
+    <CarrouselJugadoresComponent :jugadores="productos" :titulo="'PRODUCTOS DESTACADOS'" >
+
+      <template #contenido="slotProps">
+        <div v-for="item in slotProps.items" :key="item.id" class="producto-card">
+          <a class="producto-image-container" href="#/shop" style="border: 2px solid rgb(80, 194, 236);">
+            <img :src="item.imagen" :alt="item.nombre" class="producto-image">
+            <div class="hover-overlay"><b>Ver en Shop</b></div>
+          </a>
+          <h3 style="text-wrap: nowrap; margin-top: 10px;">{{ item.nombre }}</h3>
+        </div>
+      </template>
+
+    </CarrouselJugadoresComponent>
+  </div>
 </template>
 
 <script setup>
 // EL CÓDIGO FUNCIONÓ UNA VEZ ASÍ, PERO COMO LA CARGA ES TEMPRANA PUEDE QUE SEA EL MOTIVO POR EL CUAL LA PROP NOTICIAS NO LLEGA A CARGARSE
 import CarrouselNoticiasComponent from '@/components/CarrouselNoticias.vue';
 import FixtureResumenComponent from '@/components/FixtureResumen.vue';
+import CarrouselJugadoresComponent from '@/components/CarrouselJugadores.vue';
 import { ref, computed, onBeforeMount } from 'vue';
 import { useStore } from 'vuex';
 import racingClubEscudo from '@/assets/escudos/racingClub.png';
@@ -24,12 +53,23 @@ const cambiarCategoriaNoticias = (categoria, mutation) => {
   store.dispatch('web/loadNoticias', {categoria, mutation})
 };
 
+const cambiarCategoriaCatalogo = (categoria, mutation) => {
+  // console.log(`cambiarCategoria: ${categoria}, ${mutation}`);
+  store.dispatch('web/loadProductos', { categoria, mutation });
+};
+
 onBeforeMount(() => {
     cambiarCategoriaNoticias('ultimas_noticias', 'setUltimasNoticias')
+    cambiarCategoriaCatalogo('destacados', 'setProductosDestacados')
+
 })
 
 const noticias = computed(() => {
   return Object.values(store.state.web.noticiasSeleccionadas)
+});
+
+const productos = computed(() => {
+  return store.state.web.productosSeleccionados
 });
 
 const partidos = ref([
@@ -92,6 +132,69 @@ const partidos = ref([
 
 ])
 
+const jugadores = ref([
+  {
+    id: 1,
+    nombre: 'Gabriel Arias',
+    imagen: require('@/assets/imagenesJugadores/arias.jpg'),
+    dorsal: 1
+  },
+  {
+    id: 2,
+    nombre: 'Gonzalo Escudero',
+    imagen: require('@/assets/imagenesJugadores/escudero.jpg'),
+    dorsal: 43
+  },
+  {
+    id: 3,
+    nombre: 'Gabriel Rojas',
+    imagen: require('@/assets/imagenesJugadores/rojas.jpg'),
+    dorsal: 27
+  },
+  {
+    id: 4,
+    nombre: 'Juan Nardoni',
+    imagen: require('@/assets/imagenesJugadores/nardoni.jpg'),
+    dorsal: 5
+  },
+  {
+    id: 5,
+    nombre: 'Agustín Almendra',
+    imagen: require('@/assets/imagenesJugadores/almendra.jpg'),
+    dorsal: 32
+  },
+  {
+    id: 6,
+    nombre: 'Bruno Zuculini',
+    imagen: require('@/assets/imagenesJugadores/zuculini.jpg'),
+    dorsal: 36
+  },
+  {
+    id: 7,
+    nombre: 'Baltasar Rodriguez',
+    imagen: require('@/assets/imagenesJugadores/baltasar.jpg'),
+    dorsal: 22
+  },
+  {
+    id: 8,
+    nombre: 'Juan Fernando Quintero',
+    imagen: require('@/assets/imagenesJugadores/quintero.jpg'),
+    dorsal: 8
+  },
+  {
+    id: 9,
+    nombre: 'Santiago Solari',
+    imagen: require('@/assets/imagenesJugadores/solari.jpg'),
+    dorsal: 28
+  },
+  {
+    id: 10,
+    nombre: 'Roger Martinez',
+    imagen: require('@/assets/imagenesJugadores/martinez.jpg'),
+    dorsal: 9
+  }
+])
+
 </script>
 
 <style scoped>
@@ -102,19 +205,130 @@ const partidos = ref([
   gap: 30px;
 }
 
-/* Estilos para la barra lateral */
 .sidebar {
   display: flex;
   justify-content: center;
   margin-right: 30px;
 }
 
-/* Estilos para el contenido principal */
 .main-content {
-  flex-grow: 1; /* Ocupa el espacio restante */
-  max-width: 100%; /* Ajusta según sea necesario */
+  flex-grow: 1;
+  max-width: 100%;
   margin: 20px 0 20px 0;
 }
+
+.carousel-productos {
+  margin-top: 30px;
+}
+
+/* Estilos contenido de CarouselJugadoresComponent */
+
+.jugador-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 20%;
+  box-sizing: border-box;
+  padding: 10px;
+}
+
+.producto-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 20%;
+  box-sizing: border-box;
+  padding: 10px;
+}
+
+.jugador-image-container {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin: 0 auto;
+  cursor: pointer;
+  color: inherit;
+}
+
+.producto-image-container {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 0 auto;
+  cursor: pointer;
+  color: inherit;
+}
+
+.jugador-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.producto-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.hover-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(80, 194, 236, 0.5);
+  font-size: 1.5rem;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+}
+
+.jugador-image-container:hover .jugador-image, .producto-image-container:hover .producto-image {
+  opacity: 0;
+}
+
+.jugador-image-container:hover .hover-overlay, .producto-image-container:hover .hover-overlay {
+  opacity: 1;
+}
+
+@media (max-width: 1120px){
+  .jugador-image-container, .producto-image-container {
+    width: 150px;
+    height: 150px;
+  }
+  .jugador-card h3, .producto-card h3 {
+    font-size: 1em;
+  }
+}
+
+@media (max-width: 870px){
+  .jugador-card, .producto-card {
+    min-width: 25%;
+  }
+}
+
+@media (max-width: 700px){
+  .jugador-card, .producto-card {
+    min-width: 33.33%;
+  }
+}
+
+@media (max-width: 520px){
+  .jugador-card, .producto-card {
+    min-width: 50%;
+  }
+}
+
+/* Fin de estilos del contenido de CarouselJugadoresComponent */
 
 @media (max-width: 1000px){
   .home {
